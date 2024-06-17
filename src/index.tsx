a390769f57db2f74b438371b0a7cc02909ee6ee5
Vincent precious <5tcentunary@gmail.com>
2024-06-17 18:53:45 +0100

    Exported components

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
 export { default as Button } from './components/Button';
 export { default as Text } from './components/Text';
+export { default as Icon } from './components/Icon';
+export { default as BottomSheet } from './components/BottomSheet/BottomMethodSheet';
+export { default as BottomSheetInput } from './components/BottomSheet/CustomBottomSheetInput';
